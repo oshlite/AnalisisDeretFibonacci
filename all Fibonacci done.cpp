c843dc4af376d5fadf90cb422b1b0a0a15d4a6e2
lite version of me || oryza surya <140532148+oshlite@users.noreply.github.com>
2024-06-26 22:25:32 +0700

    Update all Fibonacci done.cpp

--- all Fibonacci done.cpp
+++ all Fibonacci done.cpp
@@ -3,3 +3,10 @@
 #include <iomanip>
 using namespace std;
 using namespace std::chrono;
+
+long long fibonacciRekursif(int n){
+    if (n<=1)
+        return n;
+    return fibonacciRekursif(n-1)+fibonacciRekursif(n-2);
+}
+
