ee9be036374e24265c388d644278673d6ddcce01
lite version of me || oryza surya <140532148+oshlite@users.noreply.github.com>
2024-06-26 22:25:45 +0700

    Update all Fibonacci done.cpp

--- all Fibonacci done.cpp
+++ all Fibonacci done.cpp
@@ -10,3 +10,12 @@ long long fibonacciRekursif(int n){
     return fibonacciRekursif(n-1)+fibonacciRekursif(n-2);
 }
 
+long long fibonacciDynamic(int n){
+    if (n<=1)
+        return n;
+    long long a=0,b=1,c;
+    for (int i=2; i<=n; i++){
+        c=a+b; a=b; b=c;
+    }return b;
+}
+
